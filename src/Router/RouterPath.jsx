20d398d7baa3d94126a5cacdcb586fca1a73e63b
unknown <gowthamhirthick@gmail.com>
2025-01-08 09:44:41 +0530

    change

--- src/Router/RouterPath.jsx
+++ src/Router/RouterPath.jsx
@@ -1,9 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-
-
 import Student_list from '../Components/People/Student/Student_list.jsx';
 import Courses from '../Components/Academic/Courses/Courses_list.jsx'
-
 import {Navbar} from '../Components/Home/Navbar/Navbar.jsx';
 import {Home} from '../Components/Home/Home/Home.jsx';
 
@@ -12,12 +9,9 @@ export const RouterPath = () => {
     <div>
       <Navbar/>
       <Routes>
-
-        <Route path="/" element={<h1>Testing</h1>} />
         <Route path="/Student" element={<Student_list />} />
         <Route path="/courses" element={<Courses />} />
-
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
 
       </Routes>
     </div>
