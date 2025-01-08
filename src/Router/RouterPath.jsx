55bbf898b201a7d1bfc61319ea65d592b1f67ced
jothimani <jothimani88531@gmail.com>
2025-01-08 13:21:58 +0530

    conference page

--- src/Router/RouterPath.jsx
+++ src/Router/RouterPath.jsx
@@ -3,14 +3,27 @@ import Student_list from '../Components/People/Student/Student_list.jsx';
 import Courses from '../Components/Academic/Courses/Courses_list.jsx'
 import {Navbar} from '../Components/Home/Navbar/Navbar.jsx';
 import {Home} from '../Components/Home/Home/Home.jsx';
+import  FacultyList from '../Components/People/Faculty/FacultyList.js';
+
+
+import Journals from '../Components/Publication/Journal/Journals.js';
+import Conferences from '../Components/Publication/Paper/Conferences.js';
+import Patents from '../Components/Publication/Patent/Patent.js';
+
 
 export const RouterPath = () => {
   return (
     <div>
       <Navbar/>
       <Routes>
-        <Route path="/Student" element={<Student_list />} />
-        <Route path="/courses" element={<Courses />} />
+      
+          <Route path="/Publications/journals" element={<Journals />} />
+          <Route path="/Publications/conferences" element={<Conferences />} />
+          <Route path="/Publications/patents" element={<Patents />} />
+        
+      <Route path="/Academic/Faculty" element={ <FacultyList />} />
+        <Route path="/Academic/Student" element={<Student_list />} />
+        <Route path="/Academic/courses" element={<Courses />} />
         <Route path="/" element={<Home />} />
 
       </Routes>
