bfb52aa7e953bcda53d7403cc01b3b8a46c33104
jothimani2004 <jothimani88531@gmail.com>
2025-01-09 12:31:56 +0530

    done till contact page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterPath } from "./Router/RouterPath.jsx";
 import { BrowserRouter } from "react-router-dom";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
